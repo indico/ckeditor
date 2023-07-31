778764002bc0b9b705171c1168661043b38dfca3
Adrian Moennich <adrian.moennich@cern.ch>
2023-07-31 17:29:32 +0200

    Release 1.11.0

--- build/translations/oc.js
+++ build/translations/oc.js
@@ -1,19 +1,19 @@
 !(function (o) {
   const r = (o.oc = o.oc || {});
   (r.dictionary = Object.assign(r.dictionary || {}, {
-    "%0 of %1": "",
-    Bold: "Gras",
-    Cancel: "Anullar",
-    Code: "",
-    Italic: "Italica",
-    "Remove color": "",
-    "Restore default": "",
-    Save: "Enregistrar",
-    "Show more items": "",
-    Strikethrough: "",
-    Subscript: "",
-    Superscript: "",
-    Underline: "",
+    '%0 of %1': '',
+    Bold: 'Gras',
+    Cancel: 'Anullar',
+    Code: '',
+    Italic: 'Italica',
+    'Remove color': '',
+    'Restore default': '',
+    Save: 'Enregistrar',
+    'Show more items': '',
+    Strikethrough: '',
+    Subscript: '',
+    Superscript: '',
+    Underline: '',
   })),
     (r.getPluralForm = function (o) {
       return o > 1;
